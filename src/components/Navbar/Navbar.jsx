1c539bb4ab82c0255f8f508977d8ffaf809966f4
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2025-03-31 10:26:27 +0300

    logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import { NavContainer, NavItem } from './Navbar.styled';
+import { NavContainer, NavItem, Logo } from './Navbar.styled';
+import crochet from '../../assets/images/crochet.png';
 
 const Navbar = () => {
   return (
     <NavContainer>
+      <NavItem to="/home">
+        <Logo src={crochet} alt="Logo" />
+      </NavItem>
       <NavItem to="/home">Home</NavItem>
       <NavItem to="/products">Products</NavItem>
       <NavItem to="/pattern">Pattern</NavItem>
--- src/components/Navbar/Navbar.styled.jsx
+++ src/components/Navbar/Navbar.styled.jsx
@@ -13,7 +13,7 @@ export const NavItem = styled(NavLink)`
   color: #333;
   font-weight: bold;
   font-size: 18px;
-  padding: 8px 12px;
+  padding: 8px 0px;
   transition: color 0.3s ease, background-color 0.3s ease;
 
   &.active {
@@ -21,8 +21,19 @@ export const NavItem = styled(NavLink)`
   }
 
   &:hover {
+    transform: scale(1.4);
     color: #0056b3;
     background-color: rgba(0, 123, 255, 0.1); /* Легкий підсвіт фону */
     border-radius: 5px;
+  }
+`;
+
+export const Logo = styled.img`
+  height: 40px;
+  cursor: pointer;
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: scale(1.6); /* Лише збільшення без рамки */
   }
 `;
